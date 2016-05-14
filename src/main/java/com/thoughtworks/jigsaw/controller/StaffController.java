8771ea43a4f25487dd436aee9f9396eac8cff574
Qiu Juntao <juntao.qiu@gmail.com>
2016-05-14 13:13:37 +1000

    add find one staff

--- src/main/java/com/thoughtworks/jigsaw/controller/StaffController.java
+++ src/main/java/com/thoughtworks/jigsaw/controller/StaffController.java
@@ -1,14 +1,12 @@
 package com.thoughtworks.jigsaw.controller;
 
+import com.thoughtworks.jigsaw.model.Staff;
 import com.thoughtworks.jigsaw.repo.StaffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,4 +27,11 @@ public class StaffController {
         Page result = staffRepository.findAll(new PageRequest(page, count, new Sort(direction, loginName)));
         return result.getContent();
     }
+
+
+    @RequestMapping(value="/{loginName}", method = RequestMethod.GET)
+    public Staff find(@PathVariable String loginName) {
+        return staffRepository.findByLoginName(loginName);
+    }
+
 }
--- src/main/java/com/thoughtworks/jigsaw/repo/StaffRepository.java
+++ src/main/java/com/thoughtworks/jigsaw/repo/StaffRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StaffRepository extends MongoRepository<Staff, String>, PagingAndSortingRepository<Staff, String> {
+    Staff findByLoginName(String loginName);
 }
