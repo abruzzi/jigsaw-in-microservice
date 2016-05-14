c7b68055b2955f1a3a41aff88d3c4b4a7352994b
Qiu Juntao <juntao.qiu@gmail.com>
2016-05-14 12:58:26 +1000

    add pagination for staff

--- src/main/java/com/thoughtworks/jigsaw/controller/StaffController.java
+++ src/main/java/com/thoughtworks/jigsaw/controller/StaffController.java
@@ -2,8 +2,12 @@ package com.thoughtworks.jigsaw.controller;
 
 import com.thoughtworks.jigsaw.repo.StaffRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,7 +20,13 @@ public class StaffController {
     private StaffRepository staffRepository;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List fetchAll() {
-        return staffRepository.findAll();
+    public List fetchAll(
+        @RequestParam(value = "page", defaultValue = "0", required = false) int page,
+        @RequestParam(value = "count", defaultValue = "10", required = false) int count,
+        @RequestParam(value = "order", defaultValue = "ASC", required = false) Sort.Direction direction,
+        @RequestParam(value = "sort", defaultValue = "loginName", required = false) String loginName) {
+
+        Page result = staffRepository.findAll(new PageRequest(page, count, new Sort(direction, loginName)));
+        return result.getContent();
     }
 }
--- src/main/java/com/thoughtworks/jigsaw/repo/StaffRepository.java
+++ src/main/java/com/thoughtworks/jigsaw/repo/StaffRepository.java
@@ -2,8 +2,9 @@ package com.thoughtworks.jigsaw.repo;
 
 import com.thoughtworks.jigsaw.model.Staff;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StaffRepository extends MongoRepository<Staff, String> {
+public interface StaffRepository extends MongoRepository<Staff, String>, PagingAndSortingRepository<Staff, String> {
 }
